11fcc5349d8af8e5432768b664a736bcbc34e919
Valentin Volkl <valentin.volkl@cern.ch>
2019-09-20 12:16:33 +0200

    [build infrastructure] pull initial setup from fcc-edm

--- init.sh
+++ init.sh
@@ -5,6 +5,5 @@ OS=`python $TOOLSPATH/hsf_get_platform.py --get os`
 # source FCC externals
 source /cvmfs/fcc.cern.ch/sw/views/releases/externals/94.2.0/x86_64-$OS-gcc62-opt/setup.sh
 
-export PODIO=$FCCVIEW
 
 
