48a753e5e86309bbae9486b7c720809413854fc9
Valentin Volkl <valentin.volkl@cern.ch>
2020-02-05 14:28:11 +0100

    configure github actions

--- init.sh
+++ init.sh
@@ -3,7 +3,9 @@ TOOLSPATH=/cvmfs/fcc.cern.ch/sw/0.8.3/tools/
 OS=`python $TOOLSPATH/hsf_get_platform.py --get os`
 
 # source FCC externals
-source /cvmfs/fcc.cern.ch/sw/views/releases/externals/94.2.0/x86_64-$OS-gcc62-opt/setup.sh
+source /cvmfs/fcc.cern.ch/sw/latest/setup.sh
+export PATH=/cvmfs/sft.cern.ch/lcg/releases/CMake/3.11.1-773ff/x86_64-centos7-gcc8-opt/bin/:$PATH
+
 
 
 
