67ff34bafe7520d98146b06078c20b6b36993461
Valentin Volkl <valentin.volkl@cern.ch>
2020-02-17 18:53:19 +0100

    avoid sourcing init twice in build scripts

--- .edm4hep-ci.d/compile_and_test_with_podio.sh
+++ .edm4hep-ci.d/compile_and_test_with_podio.sh
@@ -24,4 +24,10 @@ ninja install
 cd ..
 export CMAKE_PREFIX_PATH=$PWD/install:$CMAKE_PREFIX_PATH
 cd ..
-./.edm4hep-ci.d/compile_and_test.sh
+# edm4hep
+mkdir build install
+cd build
+cmake -DCMAKE_INSTALL_PREFIX=../install -DCMAKE_CXX_STANDARD=17 -G Ninja .. 
+ninja -k0
+ninja install
+ctest --output-on-failure
