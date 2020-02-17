aaef5d39c373d81210f67fb23d4dbb19dddd3093
Andre Sailer <andre.philippe.sailer@cern.ch>
2020-02-17 17:47:31 +0100

    CI: move to ninja build tool and not use more cores than allowed

--- .edm4hep-ci.d/compile_and_test.sh
+++ .edm4hep-ci.d/compile_and_test.sh
@@ -2,7 +2,7 @@
 source init.sh
 mkdir build install
 cd build
-cmake -DCMAKE_INSTALL_PREFIX=../install -DCMAKE_CXX_STANDARD=17 .. && \
-make -j4 && \
-make install && \
-ctest --output-on-failure
+cmake -DCMAKE_INSTALL_PREFIX=../install -DCMAKE_CXX_STANDARD=17 -G Ninja .. && \
+    ninja -k0 && \
+    ninja install && \
+    ctest --output-on-failure
