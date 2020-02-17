39a8d6d8c0bbfe0bda0115746240d661c188f359
Andre Sailer <andre.philippe.sailer@cern.ch>
2020-02-17 18:02:02 +0100

    CI: add diagnostic colors

--- .edm4hep-ci.d/compile_and_test.sh
+++ .edm4hep-ci.d/compile_and_test.sh
@@ -2,7 +2,7 @@
 source init.sh
 mkdir build install
 cd build
-cmake -DCMAKE_INSTALL_PREFIX=../install -DCMAKE_CXX_STANDARD=17 -G Ninja .. && \
+cmake -DCMAKE_INSTALL_PREFIX=../install -DCMAKE_CXX_STANDARD=17 -G Ninja -DCMAKE_CXX_FLAGS=" -fdiagnostics-color=always " .. && \
     ninja -k0 && \
     ninja install && \
     ctest --output-on-failure
