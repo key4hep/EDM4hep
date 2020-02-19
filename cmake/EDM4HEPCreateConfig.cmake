27215f47e03a25775273286d2336f1191a0fc9dd
Andre Sailer <andre.philippe.sailer@cern.ch>
2020-02-19 11:41:55 +0100

    CMake: install cmake files to lib/cmake/EDM4HEP, as is done for podio

--- cmake/EDM4HEPCreateConfig.cmake
+++ cmake/EDM4HEPCreateConfig.cmake
@@ -10,7 +10,7 @@ write_basic_package_version_file(
 # - Install time config and target files
 configure_package_config_file(${CMAKE_CURRENT_LIST_DIR}/EDM4HEPConfig.cmake.in
   "${PROJECT_BINARY_DIR}/EDM4HEPConfig.cmake"
-  INSTALL_DESTINATION "${CMAKE_INSTALL_LIBDIR}/../share/cmake/EDM4HEP"
+  INSTALL_DESTINATION "${CMAKE_INSTALL_LIBDIR}/cmake/EDM4HEP"
   PATH_VARS
     CMAKE_INSTALL_BINDIR
     CMAKE_INSTALL_INCLUDEDIR
@@ -21,10 +21,10 @@ configure_package_config_file(${CMAKE_CURRENT_LIST_DIR}/EDM4HEPConfig.cmake.in
 install(FILES
   "${PROJECT_BINARY_DIR}/EDM4HEPConfigVersion.cmake"
   "${PROJECT_BINARY_DIR}/EDM4HEPConfig.cmake"
-  DESTINATION "${CMAKE_INSTALL_LIBDIR}/../share/cmake/EDM4HEP"
+  DESTINATION "${CMAKE_INSTALL_LIBDIR}/cmake/EDM4HEP"
   )
 install(EXPORT EDM4HEPTargets
   NAMESPACE EDM4HEP::
-  DESTINATION "${CMAKE_INSTALL_LIBDIR}/../share/cmake/EDM4HEP"
+  DESTINATION "${CMAKE_INSTALL_LIBDIR}/cmake/EDM4HEP"
   )
 
