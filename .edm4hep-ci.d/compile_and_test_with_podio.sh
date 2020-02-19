fee82eedad8c2b74520a731a07102825337bfdae
Andre Sailer <andre.philippe.sailer@cern.ch>
2020-02-19 12:51:37 +0100

    CI: error when podio installation fails, abort when any cmake or make command fails

--- .edm4hep-ci.d/compile_and_test_with_podio.sh
+++ .edm4hep-ci.d/compile_and_test_with_podio.sh
@@ -5,9 +5,11 @@ git clone --depth 1 https://github.com/AIDASoft/podio || true
 cd podio
 mkdir build install
 cd build
-cmake -DCMAKE_CXX_STANDARD=17 -DBUILD_TESTING=OFF -DCMAKE_INSTALL_PREFIX=../install -G Ninja ..
-ninja -k0
-ninja install
+cmake -DCMAKE_CXX_STANDARD=17 -DBUILD_TESTING=OFF -DCMAKE_INSTALL_PREFIX=../install -G Ninja .. \
+      && ninja -k0 \
+      && ninja install \
+          || exit 1
+
 cd ..
 export CMAKE_PREFIX_PATH=$PWD/install:$CMAKE_PREFIX_PATH
 export PODIO=$PWD/install:$CMAKE_PREFIX_PATH
@@ -18,16 +20,16 @@ git clone --depth 1 https://github.com/HSF/TrickTrack || true
 cd TrickTrack
 mkdir build install
 cd build
-cmake -DCMAKE_CXX_STANDARD=17 -DBUILD_TESTING=OFF -DCMAKE_INSTALL_PREFIX=../install -G Ninja ..
-ninja -k0
-ninja install
+cmake -DCMAKE_CXX_STANDARD=17 -DBUILD_TESTING=OFF -DCMAKE_INSTALL_PREFIX=../install -G Ninja .. \
+      && ninja -k0 \
+      && ninja install
 cd ..
 export CMAKE_PREFIX_PATH=$PWD/install:$CMAKE_PREFIX_PATH
 cd ..
 # edm4hep
 mkdir build install
 cd build
-cmake -DCMAKE_INSTALL_PREFIX=../install -DCMAKE_CXX_STANDARD=17 -G Ninja .. 
-ninja -k0
-ninja install
-ctest --output-on-failure
+cmake -DCMAKE_INSTALL_PREFIX=../install -DCMAKE_CXX_STANDARD=17 -DCMAKE_CXX_FLAGS=" -fdiagnostics-color=always " -G Ninja ..  \
+      && ninja -k0 \
+      && ninja install \
+      && ctest --output-on-failure
