1bb8829a6dd4e3ab2d8c1c61c47936c7a4cf1941
Valentin Volkl <valentin.volkl@cern.ch>
2020-03-03 17:50:42 +0100

    fix cmake target include and add test of edm4hep cmake targets

--- .edm4hep-ci.d/compile_and_test_with_podio.sh
+++ .edm4hep-ci.d/compile_and_test_with_podio.sh
@@ -32,4 +32,12 @@ cd build
 cmake -DCMAKE_INSTALL_PREFIX=../install -DCMAKE_CXX_STANDARD=17 -DCMAKE_CXX_FLAGS=" -fdiagnostics-color=always " -G Ninja ..  \
       && ninja -k0 \
       && ninja install \
-      && ctest --output-on-failure
+      && ctest --output-on-failure || exit 1
+# test if installation can be used by external projects
+cd ..
+export CMAKE_PREFIX_PATH=$PWD/install:$CMAKE_PREFIX_PATH
+cd test/downstream-project-cmake-test
+mkdir build install
+cd build
+cmake -DCMAKE_CXX_STANDARD=17 -DCMAKE_INSTALL_PREFIX=../install -G Ninja .. \
+      && ninja -k0
---
+++ test/downstream-project-cmake-test/main.cxx
@@ -0,0 +1,12 @@
+
+///  make sure that downstream projects can include and link edm4hep properly
+
+
+
+#include "edm4hep/MCParticle.h"
+
+int main() {
+
+  auto a = edm4hep::MCParticle();
+  return 0;
+}
