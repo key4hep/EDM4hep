868c9087f0d8de1710846c8bd5b6d4825a498483
Valentin Volkl <valentin.volkl@cern.ch>
2020-03-05 15:14:45 +0100

    use cvmfs nightlies for ci

--- .edm4hep-ci.d/compile_and_test.sh
+++ .edm4hep-ci.d/compile_and_test.sh
@@ -1,8 +1,22 @@
-
 source init.sh
+
+export CMAKE_PREFIX_PATH=/cvmfs/sw-nightlies.hsf.org/key4hep/packageviews/podio/latest/x86_64-centos7-gcc8-opt:$CMAKE_PREFIX_PATH
+export CMAKE_PREFIX_PATH=/cvmfs/sw-nightlies.hsf.org/key4hep/packageviews/tricktrack/latest/x86_64-centos7-gcc8-opt:$CMAKE_PREFIX_PATH
+export CMAKE_PREFIX_PATH=/cvmfs/sw-nightlies.hsf.org/key4hep/packageviews/DD4hep/latest/x86_64-centos7-gcc8-opt:$CMAKE_PREFIX_PATH
+
+
+# edm4hep
 mkdir build install
 cd build
-cmake -DCMAKE_INSTALL_PREFIX=../install -DCMAKE_CXX_STANDARD=17 -G Ninja -DCMAKE_CXX_FLAGS=" -fdiagnostics-color=always " .. && \
-    ninja -k0 && \
-    ninja install && \
-    ctest --output-on-failure
+cmake -DCMAKE_INSTALL_PREFIX=../install -DCMAKE_CXX_STANDARD=17 -DCMAKE_CXX_FLAGS=" -fdiagnostics-color=always " -G Ninja ..  \
+      && ninja -k0 \
+      && ninja install \
+      && ctest --output-on-failure || exit 1
+# test if installation can be used by external projects
+cd ..
+export CMAKE_PREFIX_PATH=$PWD/install:$CMAKE_PREFIX_PATH
+cd test/downstream-project-cmake-test
+mkdir build install
+cd build
+cmake -DCMAKE_CXX_STANDARD=17 -DCMAKE_INSTALL_PREFIX=../install -G Ninja .. \
+      && ninja -k0
--- init.sh
+++ init.sh
@@ -1,9 +1,10 @@
 # Detect os
-TOOLSPATH=/cvmfs/fcc.cern.ch/sw/0.8.3/tools/
-OS=`python $TOOLSPATH/hsf_get_platform.py --get os`
+# Disabled as currently there is only one platform supported
+#TOOLSPATH=/cvmfs/fcc.cern.ch/sw/0.8.3/tools/
+#OS=`python $TOOLSPATH/hsf_get_platform.py --get os`
 
-# source FCC externals
-source /cvmfs/fcc.cern.ch/sw/views/releases/externals/96c_LS.0.0/x86_64-$OS-gcc8-opt/setup.sh
+# source LCG releases
+source /cvmfs/sft.cern.ch/lcg/views/LCG_96c_LS/x86_64-centos7-gcc8-opt/setup.sh
 
 # workaround for ROOT requiring a higher version of CMake than is provided by lcg
 export PATH=/cvmfs/sft.cern.ch/lcg/releases/CMake/3.11.1-773ff/x86_64-centos7-gcc8-opt/bin/:$PATH
