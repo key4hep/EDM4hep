f38effd244f59a737bc53613be7d029a8bb4ae37
Valentin Volkl <valentin.volkl@cern.ch>
2020-03-05 15:14:45 +0100

    use cvmfs nightlies for ci

--- .edm4hep-ci.d/compile_and_test_with_podio.sh
+++ .edm4hep-ci.d/compile_and_test_with_podio.sh
@@ -1,31 +1,5 @@
 source init.sh
 
-# podio
-git clone --depth 1 https://github.com/AIDASoft/podio || true
-cd podio
-mkdir build install
-cd build
-cmake -DCMAKE_CXX_STANDARD=17 -DBUILD_TESTING=OFF -DCMAKE_INSTALL_PREFIX=../install -G Ninja .. \
-      && ninja -k0 \
-      && ninja install \
-          || exit 1
-
-cd ..
-export CMAKE_PREFIX_PATH=$PWD/install:$CMAKE_PREFIX_PATH
-export PODIO=$PWD/install:$CMAKE_PREFIX_PATH
-cd ..
-
-# tricktrack
-git clone --depth 1 https://github.com/HSF/TrickTrack || true
-cd TrickTrack
-mkdir build install
-cd build
-cmake -DCMAKE_CXX_STANDARD=17 -DBUILD_TESTING=OFF -DCMAKE_INSTALL_PREFIX=../install -G Ninja .. \
-      && ninja -k0 \
-      && ninja install
-cd ..
-export CMAKE_PREFIX_PATH=$PWD/install:$CMAKE_PREFIX_PATH
-cd ..
 # edm4hep
 mkdir build install
 cd build
