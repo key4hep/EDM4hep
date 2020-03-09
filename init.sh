63be7e7b766aa10cd0514198c06782a6760b6d0d
Valentin Volkl <valentin.volkl@cern.ch>
2020-03-09 13:15:38 +0100

    use default values for variables in init.sh

--- init.sh
+++ init.sh
@@ -4,11 +4,8 @@
 #OS=`python $TOOLSPATH/hsf_get_platform.py --get os`
 
 # source LCG releases
-export LCG_VERSION=96c_LS
-export OS=centos7
-export COMPILER_VERSION=gcc8
-export BINARY_TAG=x86_64-$OS-$COMPILER_VERSION-opt
-source /cvmfs/sft.cern.ch/lcg/views/LCG_${LCG_VERSION}/$BINARY_TAG/setup.sh
+export BINARY_TAG=x86_64-${OS-centos7}-${COMPILER_VERSION-gcc8}-opt
+source /cvmfs/sft.cern.ch/lcg/views/${LCG_RELEASE-LCG_96c_LS}/$BINARY_TAG/setup.sh
 
 # workaround for ROOT requiring a higher version of CMake than is provided by lcg
 export PATH=/cvmfs/sft.cern.ch/lcg/releases/CMake/3.14.3-34449/$BINARY_TAG/bin/:$PATH
