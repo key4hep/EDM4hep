62ad195e3c7ea39196cd24a38f2625770ccf877d
Valentin Volkl <valentin.volkl@cern.ch>
2020-03-09 12:31:16 +0100

    refactor paths for CI setup

--- .edm4hep-ci.d/compile_and_test.sh
+++ .edm4hep-ci.d/compile_and_test.sh
@@ -1,15 +1,18 @@
 source init.sh
 
-export CMAKE_PREFIX_PATH=/cvmfs/sw-nightlies.hsf.org/key4hep/packageviews/podio/latest/x86_64-centos7-gcc8-opt:$CMAKE_PREFIX_PATH
-export CMAKE_PREFIX_PATH=/cvmfs/sw-nightlies.hsf.org/key4hep/packageviews/tricktrack/latest/x86_64-centos7-gcc8-opt:$CMAKE_PREFIX_PATH
-export CMAKE_PREFIX_PATH=/cvmfs/sw-nightlies.hsf.org/key4hep/packageviews/DD4hep/latest/x86_64-centos7-gcc8-opt:$CMAKE_PREFIX_PATH
-source /cvmfs/sw-nightlies.hsf.org/key4hep/packageviews/DD4hep/latest/x86_64-centos7-gcc8-opt/bin/thisdd4hep.sh
+export K4PACKAGEVIEW=/cvmfs/sw-nightlies.hsf.org/key4hep/packageviews
+export BINARY_TAG=x86_64-centos7-gcc8-opt
+
+export CMAKE_PREFIX_PATH=${K4PACKAGEVIEW}/podio/latest/${BINARY_TAG}:$CMAKE_PREFIX_PATH
+export CMAKE_PREFIX_PATH=${K4PACKAGEVIEW}/tricktrack/latest/${BINARY_TAG}:$CMAKE_PREFIX_PATH
+export CMAKE_PREFIX_PATH=${K4PACKAGEVIEW}/DD4hep/latest/${BINARY_TAG}:$CMAKE_PREFIX_PATH
+source ${K4PACKAGEVIEW}/DD4hep/latest/$BINARY_TAG/bin/thisdd4hep.sh
 
 
 # edm4hep
 mkdir build install
 cd build
-cmake -DCMAKE_INSTALL_PREFIX=../install -DCMAKE_CXX_STANDARD=17 -DCMAKE_CXX_FLAGS=" -fdiagnostics-color=always " -G Ninja ..  \
+cmake -DCMAKE_INSTALL_PREFIX=../install -DCMAKE_CXX_STANDARD=${STANDARD} -DCMAKE_CXX_FLAGS=" -fdiagnostics-color=always " -G Ninja ..  \
       && ninja -k0 \
       && ninja install \
       && ctest --output-on-failure || exit 1
@@ -19,5 +22,5 @@ export CMAKE_PREFIX_PATH=$PWD/install:$CMAKE_PREFIX_PATH
 cd test/downstream-project-cmake-test
 mkdir build install
 cd build
-cmake -DCMAKE_CXX_STANDARD=17 -DCMAKE_INSTALL_PREFIX=../install -G Ninja .. \
+cmake -DCMAKE_CXX_STANDARD=${STANDARD} -DCMAKE_INSTALL_PREFIX=../install -G Ninja .. \
       && ninja -k0
