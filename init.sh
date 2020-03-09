57f126d124470016fce8420f4adbe43f96f5d97d
Valentin Volkl <valentin.volkl@cern.ch>
2020-03-09 12:36:33 +0100

    refactor paths for init.sh

--- init.sh
+++ init.sh
@@ -4,10 +4,14 @@
 #OS=`python $TOOLSPATH/hsf_get_platform.py --get os`
 
 # source LCG releases
-source /cvmfs/sft.cern.ch/lcg/views/LCG_96c_LS/x86_64-centos7-gcc8-opt/setup.sh
+export LCG_VERSION=96c_LS
+export OS=centos7
+export COMPILER_VERSION=gcc8
+export BINARY_TAG=x86_64-$OS-$COMPILER_VERSION
+source /cvmfs/sft.cern.ch/lcg/views/LCG_${LCG_VERSION}/$BINARY_TAG/setup.sh
 
 # workaround for ROOT requiring a higher version of CMake than is provided by lcg
-export PATH=/cvmfs/sft.cern.ch/lcg/releases/CMake/3.14.3-34449/x86_64-centos7-gcc8-opt/bin/:$PATH
+export PATH=/cvmfs/sft.cern.ch/lcg/releases/CMake/3.14.3-34449/$BINARY_TAG/bin/:$PATH
 
 
 
