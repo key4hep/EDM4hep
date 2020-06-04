bc435d74f13828caf3bfdb2c8e20b84c1db45584
tmadlener <thomas.madlener@desy.de>
2020-06-04 21:44:47 +0200

    Add some examples

---
+++ plugins/delphes/examples/README.md
@@ -0,0 +1,80 @@
+# Testing Delphes -> EDM4hep
+
+
+## Setting up tests
+
+The following environment variables are expected to be set for the following to
+work:
+- `DELPHES_DIR`: points to the Delphes install directory
+- `EDM4HEP_DIR`: points to the EDM4hep install directory
+- `ROOT_INCLUDE_PATH`: contains the podio, EDM4hep and Delphes include
+  directories (e.g. via `$EDM4HEP_DIR/include`)
+- `LD_LIBRARY_PATH`: contains the podio, EDM4hep and Delphes libs (and dicts)
+  (e.g. via `$EDM4HEP_DIR/lib`)
+
+
+### Setting up a test folder
+
+To easily run some tests, a folder is set up and some of the Delphes examples
+are copied such that they can be easily changed without risking messing up the
+Delphes installation somehow (however unlikely).
+
+    mkdir test_delphes && cd test_delphes
+    mkdir cards inputs
+    cp ${DELPHES_DIR}/cards/delphes_card_{ILD,CMS}.tcl cards
+    cp ${DELPHES_DIR}/examples/Pythia8/configNoLHE.cmnd inputs
+
+To retrieve comparable outputs the random seeds for the Delphes card and the
+Pythia8 configuration file are fixed. Additionally a restriction to 1000 event
+is put in place. This means that
+
+    set MaxEvents 1000
+    set RandomSeed 123
+
+is added at the top of `cards/delphes_card_CMS.tcl` (resp.
+`cards/delphes_card_ILD.tcl`) and
+
+    Random:setSeed = on
+    Random:seed = 12345
+
+is added in the first section (`1) Settings used in the main program`) of
+`inputs/configNoLHE.cmnd`
+
+
+## Generating Delphes output
+
+A Delhpes output file is generated to have a baseline comparison for the edm4hep
+output. The output is generated using the following command
+
+    ${DELPHES_DIR}/bin/DelphesPythia8 \
+        cards/delphes_card_CMS.tcl \
+        inputs/configNoLHE.cmnd \
+        delphes_test_output.root
+
+
+## Generating edm4hep output
+
+The corresponding edm4hep output is produced using the following command
+
+    ${EDM4HEP_DIR}/bin/DelphesPythia8_EDM4HEP \
+        cards/delphes_card_CMS.tcl \
+        inputs/configNoLHE.cmnd \
+        edm4hep_test_output.root
+
+
+## Generating comparison histograms
+
+Two example macros that produce the same histograms once from a Delphes output
+file and once from a edm4hep output file are in
+`EDM4hep/plugins/delphes/examples`. Both take as input parameters the
+corresponding filename and produce a new `.root` file with some histograms that
+should ideally be equivalent (but are currently not).
+
+    root -q 'path/to/EDM4hep/plugins/delphes/examples/read_delphes.C("delphes_test_output.root")'
+    root -q 'path/to/EDM4hep/plugins/delphes/examples/read_edm4hep.C("edm4hep_test_output.root")' 
+
+After this the files `histograms_delphes.root` and `histograms_edm4hep.root`
+should be present and filled with some histograms that can be compared. The
+differences in the Jet energy histogram are caused by the slightly different
+ways of how masses for Jets and other Candidates are handled by Delphes (see
+comment in [DelphesMain.h](../DelphesMain.h#L88-L107)).
---
+++ plugins/delphes/examples/read_edm4hep.C
@@ -0,0 +1,133 @@
+// Make sure these can all be found on LD_LIBRARY_PATH
+#ifdef __CLING__
+R__LOAD_LIBRARY(libpodio)
+R__LOAD_LIBRARY(libpodioDict)
+R__LOAD_LIBRARY(libpodioRootIO)
+R__LOAD_LIBRARY(libedm4hep)
+R__LOAD_LIBRARY(libedm4hepDict)
+#endif
+
+#include "AssociationHelper.h"
+#include "common_helpers.h"
+
+// make sure that the EDM4hep include directory is in ROOT_INCLUDE_PATH
+#include "edm4hep/MCParticleCollection.h"
+#include "edm4hep/ReconstructedParticleCollection.h"
+#include "edm4hep/MCRecoParticleAssociationCollection.h"
+
+// also make sure that the podio include directory is in ROOT_INCLUDE_PATH
+#include "podio/EventStore.h"
+#include "podio/ROOTReader.h"
+
+#include "Math/Vector4D.h"
+
+#include <string>
+#include <iostream>
+#include <cmath>
+
+using LorentzVectorT = ROOT::Math::PxPyPzEVector;
+
+struct BasicInfo {
+    double pt;
+    double E;
+};
+
+
+LorentzVectorT getGen4Momentum(edm4hep::ReconstructedParticle const& recoCand,
+                            MCRecoParticleAssociationNavigator const& relationNavigator)
+{
+    const auto mcParticles = relationNavigator.getSim(recoCand);
+
+    LorentzVectorT momentum{};
+    // NOTE: for the moment using all the related particles here. This should
+    // not make a difference for the current implementation of the conversion
+    // since Jets should be the only ones which possibly have more then one
+    // related MCParticle
+    for (const auto& mcParticle : mcParticles) {
+        const auto& partMom = mcParticle.getMomentum();
+        LorentzVectorT tmp;
+        tmp.SetCoordinates(partMom[0], partMom[1], partMom[2], mcParticle.getEnergy());
+        momentum += tmp;
+    }
+
+    return momentum;
+}
+
+BasicInfo getDeltaInfo(edm4hep::ReconstructedParticle const& recoCand,
+                       MCRecoParticleAssociationNavigator const& relationNavigator) {
+
+    const auto gen4Momentum = getGen4Momentum(recoCand, relationNavigator);
+    if (gen4Momentum == LorentzVectorT()) {
+        return {-1, 1};
+    }
+
+    const auto& recoMomentum = recoCand.getMomentum();
+    const double pt = std::sqrt(recoMomentum[0] * recoMomentum[0] + recoMomentum[1] * recoMomentum[1]);
+
+    return BasicInfo{pt - gen4Momentum.Pt(), recoCand.getEnergy() - gen4Momentum.E()};
+}
+
+
+void fillHists(TH1F* hDeltaPt, TH1F* hDeltaE, edm4hep::ReconstructedParticle const& recoCand,
+               MCRecoParticleAssociationNavigator const& relationNavigator) {
+    const auto deltaInfo = getDeltaInfo(recoCand, relationNavigator);
+    hDeltaPt->Fill(deltaInfo.pt);
+    hDeltaE->Fill(deltaInfo.E);
+}
+
+
+void read_edm4hep(std::string&& inputfile) {
+    gSystem->Load("libpodio.so");
+    gSystem->Load("libpodioDict.so");
+    gSystem->Load("libpodioRootIO.so");
+    gSystem->Load("libedm4hep.so");
+    gSystem->Load("libedm4hepDict.so");
+
+    auto reader = podio::ROOTReader();
+    reader.openFile(inputfile);
+    auto store = podio::EventStore();
+    store.setReader(&reader);
+
+    defineHists();
+
+    const auto nEntries = reader.getEntries();
+    for (int entry = 0; entry < nEntries; ++entry) {
+
+        auto& genParticles = store.get<edm4hep::MCParticleCollection>("MCParticles");
+        auto& muons = store.get<edm4hep::ReconstructedParticleCollection>("Muon");
+        auto& electrons = store.get<edm4hep::ReconstructedParticleCollection>("Electron");
+        auto& photons = store.get<edm4hep::ReconstructedParticleCollection>("Photon");
+        auto& jets = store.get<edm4hep::ReconstructedParticleCollection>("Jet");
+        auto& recoMCAssociations = store.get<edm4hep::MCRecoParticleAssociationCollection>("MCRecoAssociations");
+
+        MCRecoParticleAssociationNavigator mcRecoNavigator(recoMCAssociations);
+
+        for (const auto& muon : muons) {
+            fillHists(muonDeltaPt, muonDeltaE, muon, mcRecoNavigator);
+        }
+
+        for (const auto& electron : electrons) {
+            fillHists(electronDeltaPt, electronDeltaE, electron, mcRecoNavigator);
+        }
+
+        for (const auto& photon : photons) {
+            fillHists(photonDeltaPt, photonDeltaE, photon, mcRecoNavigator);
+        }
+
+        for (const auto& jet : jets) {
+            fillHists(jetDeltaPt, jetDeltaE, jet, mcRecoNavigator);
+
+            const auto gen4Momentum = getGen4Momentum(jet, mcRecoNavigator);
+            jetRecoE->Fill(jet.getEnergy());
+            jetGenE->Fill(gen4Momentum.E());
+            jetRecoM->Fill(jet.getMass());
+            jetGenM->Fill(gen4Momentum.M());
+        }
+
+
+        store.clear();
+        reader.endOfEvent();
+    }
+
+    storeHists("histograms_edm4hep.root");
+}
