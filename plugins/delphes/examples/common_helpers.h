e01da752edd23dab1308180d9a616863cc41c4e0
tmadlener <thomas.madlener@desy.de>
2020-06-04 21:44:47 +0200

    Add some examples

---
+++ plugins/delphes/examples/common_helpers.h
@@ -0,0 +1,72 @@
+#include "TH1F.h"
+#include "TH1F.h"
+
+#include <cmath>
+#include <vector>
+#include <string>
+
+// declared globally in this header, but it's just a small demonstrator for now
+// and it saves some duplication
+TH1F* muonDeltaPt;
+TH1F* muonDeltaE;
+TH1F* electronDeltaPt;
+TH1F* electronDeltaE;
+TH1F* photonDeltaPt;
+TH1F* photonDeltaE;
+TH1F* jetDeltaPt;
+TH1F* jetDeltaE;
+TH1F* jetRecoE;
+TH1F* jetGenE;
+TH1F* jetRecoM;
+TH1F* jetGenM;
+
+TH1F* logXScaleBinned(const char* name, const int nBins, const float min, const float max)
+{
+    std::vector<double> xBinning;
+    xBinning.reserve(nBins + 1);
+
+    const float lmin = std::log10(min);
+    const float lmax = std::log10(max);
+    const float dx = (lmax - lmin) / nBins;
+    const float l10 = std::log(10);
+
+    for (int i = 0; i < nBins + 1; ++i) {
+        xBinning.push_back(lmin + std::exp(l10 * i * dx));
+    }
+
+    return new TH1F(name, name, nBins, xBinning.data());
+}
+
+void defineHists() {
+    muonDeltaPt = new TH1F("muonDeltaPt", "muonDeltaPt", 100, -10, 10);
+    muonDeltaE = new TH1F("muonDeltaE", "muonDeltaE", 100, -10, 10);
+    electronDeltaPt = new TH1F("electronDeltaPt", "electronDeltaPt", 100, -10, 10);
+    electronDeltaE = new TH1F("electronDeltaE", "electronDeltaE", 100, -10, 10);
+    photonDeltaPt = new TH1F("photonDeltaPt", "photonDeltaPt", 100, -10, 10);
+    photonDeltaE = new TH1F("photonDeltaE", "photonDeltaE", 100, -10, 10);
+    jetDeltaPt = new TH1F("jetDeltaPt", "jetDeltaPt", 100, -10, 10);
+    jetDeltaE = new TH1F("jetDeltaE", "jetDeltaE", 100, -10, 10);
+    jetRecoE = logXScaleBinned("jetRecoE", 100, 2, 2000);
+    jetGenE = logXScaleBinned("jetGenE", 100, 2, 2000);
+    jetRecoM = new TH1F("jetRecoM", "jetRecoM", 100, 2, 150);
+    jetGenM = new TH1F("jetGenM", "jetGenM", 100, 2, 150);
+}
+
+void storeHists(const char* filename) {
+    auto* file = new TFile(filename, "recreate");
+    file->cd();
+
+    muonDeltaPt->Write();
+    muonDeltaE->Write();
+    electronDeltaPt->Write();
+    electronDeltaE->Write();
+    photonDeltaPt->Write();
+    photonDeltaE->Write();
+    jetDeltaPt->Write();
+    jetDeltaE->Write();
+    jetRecoE->Write();
+    jetGenE->Write();
+    jetRecoM->Write();
+    jetGenM->Write();
+    file->Close();
+}
---
+++ plugins/delphes/examples/read_delphes.C
@@ -0,0 +1,133 @@
+// make sure that libDelphes can be found in LD_LIBARARY_PATH
+#ifdef __CLING__
+R__LOAD_LIBRARY(libDelphes)
+#endif
+
+#include "common_helpers.h"
+
+// make sure that the Delphes include directory is in ROOT_INCLUDE_PATH
+#include "classes/DelphesClasses.h"
+#include "ExRootAnalysis/ExRootTreeReader.h"
+
+#include "TClonesArray.h"
+#include "TChain.h"
+#include "TH1F.h"
+#include "Math/Vector4D.h"
+
+#include <cmath>
+#include <vector>
+
+using LorentzVectorT = ROOT::Math::PtEtaPhiEVector;
+
+struct BasicInfo {
+    double pt;
+    double E;
+};
+
+template<typename T>
+BasicInfo getBasicInfo(const T* cand) {
+    return {cand->PT, cand->P4().E()};
+}
+
+template<typename RecoT>
+BasicInfo getDeltaInfo(const RecoT* recoCand, const GenParticle* genCand) {
+    const auto recoInfo = getBasicInfo(recoCand);
+    const auto mcInfo = getBasicInfo(genCand);
+    return {recoInfo.pt - mcInfo.pt, recoInfo.E - mcInfo.E};
+}
+
+template<typename RecoT>
+BasicInfo getDeltaInfo(const RecoT* recoCand) {
+    auto* mcCand = dynamic_cast<GenParticle*>(recoCand->Particle.GetObject());
+    if (mcCand) {
+        return getDeltaInfo(recoCand, mcCand);
+    }
+
+    return {-1, -1};
+}
+
+BasicInfo getDeltaInfo(const Photon* recoCand) {
+    // don't handle cases where there is more than 1 related particle
+    if (recoCand->Particles.GetEntriesFast() == 1) {
+        auto* mcCand = dynamic_cast<GenParticle*>(recoCand->Particles.At(0));
+        return getDeltaInfo(recoCand, mcCand);
+    }
+    return {-1, -1};
+}
+
+LorentzVectorT getJetGen4Momentum(const Jet* jet) {
+    LorentzVectorT momentum{};
+
+    // sum up all of the associated particles
+    for (int i = 0; i < jet->Particles.GetEntriesFast(); ++i) {
+        const auto* particle = static_cast<GenParticle*>(jet->Particles.At(i));
+        LorentzVectorT tmpMomentum{};
+        tmpMomentum.SetCoordinates(particle->PT, particle->Eta, particle->Phi, particle->P4().E());
+        momentum += tmpMomentum;
+    }
+    return momentum;
+}
+
+
+BasicInfo getDeltaInfo(const Jet* recoCand) {
+    const auto momentum = getJetGen4Momentum(recoCand);
+    return {recoCand->P4().Pt() - momentum.Pt(), recoCand->P4().E() - momentum.E()};
+}
+
+template<typename RecoT>
+void fillHists(TH1F* hDeltaPt, TH1F* hDeltaE, RecoT* recoCand) {
+    const auto deltaInfo = getDeltaInfo(recoCand);
+    hDeltaPt->Fill(deltaInfo.pt);
+    hDeltaE->Fill(deltaInfo.E);
+}
+
+
+// Following delphes/examples/Example3.C (more or less)
+void read_delphes(const char* inputfile) {
+    gSystem->Load("libDelphes.so");
+
+    auto* chain = new TChain("Delphes");
+    chain->Add(inputfile);
+    auto* treeReader = new ExRootTreeReader(chain);
+    const auto nEntries = treeReader->GetEntries();
+
+    auto* genParticles = treeReader->UseBranch("Particle");
+    auto* electrons = treeReader->UseBranch("Electron");
+    auto* muons = treeReader->UseBranch("Muon");
+    auto* photons = treeReader->UseBranch("Photon");
+    auto* jets = treeReader->UseBranch("Jet");
+
+    defineHists();
+
+
+    for (int entry = 0; entry < nEntries; ++entry) {
+        treeReader->ReadEntry(entry);
+
+        // std::cout << genParticles->GetEntries() << "\n";
+
+        for (int i = 0; i < muons->GetEntries(); ++i) {
+            fillHists(muonDeltaPt, muonDeltaE, static_cast<Muon*>(muons->At(i)));
+        }
+
+        for (int i = 0; i < electrons->GetEntries(); ++i) {
+            fillHists(electronDeltaPt, electronDeltaE, static_cast<Electron*>(electrons->At(i)));
+        }
+
+        for (int i = 0; i < photons->GetEntries(); ++i) {
+            fillHists(photonDeltaPt, photonDeltaE, static_cast<Photon*>(photons->At(i)));
+        }
+
+        for (int i = 0; i < jets->GetEntries(); ++i) {
+            const auto* jet = static_cast<Jet*>(jets->At(i));
+            fillHists(jetDeltaPt, jetDeltaE, jet);
+
+            const auto gen4momentum = getJetGen4Momentum(jet);
+            jetRecoE->Fill(jet->P4().E());
+            jetGenE->Fill(gen4momentum.E());
+            jetRecoM->Fill(jet->Mass);
+            jetGenM->Fill(gen4momentum.M());
+        }
+    }
+
+    storeHists("histograms_delphes.root");
+}
