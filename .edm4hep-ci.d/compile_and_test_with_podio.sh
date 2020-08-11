21d0cafdacfcfe17826cf0b1a2d6fdec202039af
Valentin Volkl <valentin.volkl@cern.ch>
2020-08-11 10:54:06 +0200

    ci: add step with podio master
    allow failure for key4hep-nightlies
    
    up
    
    up
    
    up2
    
    up3
    
    up

--- .edm4hep-ci.d/compile_and_test_with_podio.sh
+++ .edm4hep-ci.d/compile_and_test_with_podio.sh
@@ -1,18 +1,16 @@
-source init.sh
 
-# edm4hep
+# podio
+git clone --depth 1 https://github.com/AIDASoft/podio || true
+cd podio
 mkdir build install
 cd build
-cmake -DCMAKE_INSTALL_PREFIX=../install -DCMAKE_CXX_STANDARD=17 -DEDM4HEP_DOCUMENTATION=ON -DCMAKE_CXX_FLAGS=" -fdiagnostics-color=always " -G Ninja ..  \
+cmake -DCMAKE_CXX_STANDARD=17 -DBUILD_TESTING=OFF -DCMAKE_INSTALL_PREFIX=../install -G Ninja .. \
       && ninja -k0 \
-      && ninja doc \
       && ninja install \
-      && ctest --output-on-failure || exit 1
-# test if installation can be used by external projects
+          || exit 1
+
 cd ..
 export CMAKE_PREFIX_PATH=$PWD/install:$CMAKE_PREFIX_PATH
-cd test/downstream-project-cmake-test
-mkdir build install
-cd build
-cmake -DCMAKE_CXX_STANDARD=17 -DCMAKE_INSTALL_PREFIX=../install -G Ninja .. \
-      && ninja -k0
+export PODIO=$PWD/install:$CMAKE_PREFIX_PATH
+cd ..
+
