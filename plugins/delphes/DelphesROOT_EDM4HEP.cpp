2cb89d0008fbf112636b8284cff74c997684c974
vvolkl <valentin.volkl@cern.ch>
2020-11-10 09:54:23 +0100

    remove delphes, (migrated to k4simdelphes) (#94)

--- plugins/delphes/DelphesROOT_EDM4HEP.cpp
+++
@@ -1,8 +0,0 @@
-
-#include "DelphesMain.h"
-#include "DelphesRootReader.h"
-
-int main(int argc, char *argv[]) {
-  DelphesRootReader inputReader = DelphesRootReader();
-  return doit(argc, argv, inputReader);
-}
