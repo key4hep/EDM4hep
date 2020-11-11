af6eb03f9504d318c352520da188541b8189e2f6
tmadlener <thomas.madlener@desy.de>
2020-11-11 10:44:09 +0100

    Enable SIO support in podio when built on the fly

--- .edm4hep-ci.d/compile_and_test_with_podio.sh
+++ .edm4hep-ci.d/compile_and_test_with_podio.sh
@@ -4,7 +4,7 @@ git clone --depth 1 https://github.com/AIDASoft/podio || true
 cd podio
 mkdir build install
 cd build
-cmake -DCMAKE_CXX_STANDARD=17 -DBUILD_TESTING=OFF -DCMAKE_INSTALL_PREFIX=../install -G Ninja .. \
+cmake -DCMAKE_CXX_STANDARD=17 -DBUILD_TESTING=OFF -DCMAKE_INSTALL_PREFIX=../install -DENABLE_SIO=ON -G Ninja .. \
       && ninja -k0 \
       && ninja install \
           || exit 1
