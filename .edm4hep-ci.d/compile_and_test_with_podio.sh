67e39b9b635ac7b2458e9ab3244c941787f0ed26
tmadlener <thomas.madlener@desy.de>
2020-11-11 11:58:02 +0100

    Try to pick up podio that has just been built
    Need to update some environment variables. CPATH has to be unset, as
    otherwise it would interfere with the build process, as it points to the
    centrally installed podio. Pushing it to ROOT_INCLUDE_PATH is necessary
    for the DD4Hep test to run.

--- .edm4hep-ci.d/compile_and_test_with_podio.sh
+++ .edm4hep-ci.d/compile_and_test_with_podio.sh
@@ -4,15 +4,16 @@ git clone --depth 1 https://github.com/AIDASoft/podio || true
 cd podio
 mkdir build install
 cd build
-cmake -DCMAKE_CXX_STANDARD=17 -DBUILD_TESTING=OFF -DCMAKE_INSTALL_PREFIX=../install -DENABLE_SIO=ON -G Ninja .. \
+cmake -DCMAKE_CXX_STANDARD=17 -DBUILD_TESTING=OFF -DCMAKE_INSTALL_PREFIX=$PWD/../install -DENABLE_SIO=ON -G Ninja .. \
       && ninja -k0 \
       && ninja install \
           || exit 1
 
 cd ..
+# Place this installation in front of any other which might be present in the
+# setup
+export ROOT_INCLUDE_PATH=$PWD/install/include:$ROOT_INCLUDE_PATH:$CPATH
+unset CPATH
 export CMAKE_PREFIX_PATH=$PWD/install:$CMAKE_PREFIX_PATH
-export PODIO=$PWD/install:$CMAKE_PREFIX_PATH
 export LD_LIBRARY_PATH=$PWD/install/lib:$PWD/install/lib64:$LD_LIBRARY_PATH
-export ROOT_INCLUDE_PATH=$PWD/install/include:$ROOT_INCLUDE_PATH
 cd ..
-
