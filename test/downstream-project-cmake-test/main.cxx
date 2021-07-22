6552734200419ab29bcddb0e81c01a96c3d66d56
Thomas Madlener <thomas.madlener@desy.de>
2021-07-22 15:37:07 +0200

    Add utilities to downstream package tests

--- test/downstream-project-cmake-test/main.cxx
+++ test/downstream-project-cmake-test/main.cxx
@@ -1,12 +1,14 @@
-
 ///  make sure that downstream projects can include and link edm4hep properly
 
 
 
 #include "edm4hep/MCParticle.h"
+#include "edm4hep/kinematics.h"
 
 int main() {
 
   auto a = edm4hep::MCParticle();
+  const auto p4 = edm4hep::utils::p4(a);
+
   return 0;
 }
