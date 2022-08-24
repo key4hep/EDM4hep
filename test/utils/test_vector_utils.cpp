821ba79cecbfe36f8870a5c85f09a4cf00de6dd8
Wouter Deconinck <wdconinc@gmail.com>
2022-08-24 03:40:36 -0500

    Utilities for vector components (#149)
    Co-authored-by: Thomas Madlener <thomas.madlener@desy.de>

---
+++ test/utils/test_vector_utils.cpp
@@ -0,0 +1,121 @@
+#include <catch2/catch_approx.hpp>
+#include <catch2/catch_template_test_macros.hpp>
+#include <catch2/catch_test_macros.hpp>
+
+#include "edm4hep/utils/vector_utils.h"
+
+#include "edm4hep/Vector2f.h"
+#include "edm4hep/Vector2i.h"
+#include "edm4hep/Vector3d.h"
+#include "edm4hep/Vector3f.h"
+
+#include <tuple>
+#include <type_traits>
+
+using AllVectorTypes = std::tuple<edm4hep::Vector3f, edm4hep::Vector3d, edm4hep::Vector2i, edm4hep::Vector2f>;
+
+template <typename V>
+constexpr V create();
+
+template <>
+constexpr edm4hep::Vector3f create() {
+  return edm4hep::Vector3f{1.0f, 2.0f, 3.0f};
+}
+
+template <>
+constexpr edm4hep::Vector3d create() {
+  return edm4hep::Vector3d{1.0, 2.0, 3.0};
+}
+
+template <>
+constexpr edm4hep::Vector2f create() {
+  return edm4hep::Vector2f{1.0f, 2.0f};
+}
+
+template <>
+constexpr edm4hep::Vector2i create() {
+  return edm4hep::Vector2i{1, 2};
+}
+
+TEMPLATE_LIST_TEST_CASE("Vector uniform getters", "[vector_utils]", AllVectorTypes) {
+  using namespace edm4hep;
+
+  constexpr auto vector = create<TestType>();
+
+  STATIC_REQUIRE(utils::vector_x(vector) == utils::ValueType<TestType>(1.0));
+  STATIC_REQUIRE(utils::vector_y(vector) == utils::ValueType<TestType>(2.0));
+  // 2D vectors fill z component with 0
+  if constexpr (std::is_same_v<TestType, edm4hep::Vector2i> || std::is_same_v<TestType, edm4hep::Vector2f>) {
+    STATIC_REQUIRE(utils::vector_z(vector) == utils::ValueType<TestType>(0.0));
+  } else {
+    STATIC_REQUIRE(utils::vector_z(vector) == utils::ValueType<TestType>(3.0));
+  }
+}
+
+TEST_CASE("Vector ValueType", "[vector_utils]") {
+  using namespace edm4hep;
+  STATIC_REQUIRE(std::is_same_v<int32_t, utils::ValueType<Vector2i>>);
+  STATIC_REQUIRE(std::is_same_v<float, utils::ValueType<Vector2f>>);
+  STATIC_REQUIRE(std::is_same_v<float, utils::ValueType<Vector3f>>);
+  STATIC_REQUIRE(std::is_same_v<double, utils::ValueType<Vector3d>>);
+}
+
+TEMPLATE_LIST_TEST_CASE("Vector operators", "[vector_utils]", AllVectorTypes) {
+  using namespace edm4hep;
+
+  constexpr auto vector1 = create<TestType>();
+  constexpr auto vector2 = create<TestType>();
+
+  // Some very basic tests to check addition and multiplication / division by a factor
+  constexpr auto sumV = vector1 + vector2;
+  STATIC_REQUIRE(sumV == 2 * vector1);
+  STATIC_REQUIRE(sumV == vector1 * 2); // check that both orders of args work
+  STATIC_REQUIRE(sumV / 2 == vector2);
+
+  // check that subtraction works
+  STATIC_REQUIRE(sumV - vector1 == vector2);
+
+  // Vector product (depends again on whether it is 2D or 3D)
+  if constexpr (std::is_same_v<TestType, edm4hep::Vector2i> || std::is_same_v<TestType, edm4hep::Vector2f>) {
+    STATIC_REQUIRE(vector1 * vector2 == utils::ValueType<TestType>(5));
+  } else {
+    STATIC_REQUIRE(vector1 * vector2 == utils::ValueType<TestType>(14));
+  }
+}
+
+TEMPLATE_LIST_TEST_CASE("Vector utility functionality", "[vector_utils]", AllVectorTypes) {
+  const auto vector = create<TestType>();
+
+  using namespace edm4hep;
+
+  // Can only normalize vectors with floating point numbers
+  if constexpr (!std::is_same_v<TestType, Vector2i>) {
+    const auto normV = utils::normalizeVector(vector);
+    REQUIRE(utils::magnitude(normV) == Catch::Approx(1));
+    REQUIRE(utils::projection(normV, vector) == Catch::Approx(1));
+  }
+
+  // Small differences in expectations between 2D and 3D vectors for everything
+  // that involves the z component
+  if constexpr (std::is_same_v<TestType, Vector2i> || std::is_same_v<TestType, Vector2f>) {
+    REQUIRE(utils::magnitude(vector) == Catch::Approx(std::sqrt(5)));
+
+    const auto otherVec = TestType(3, 4);
+    REQUIRE(utils::angleBetween(otherVec, vector) == Catch::Approx(0.17985349)); // acos(11 / (5 * sqrt(5)))
+  } else {
+    REQUIRE(utils::magnitude(vector) == Catch::Approx(std::sqrt(14)));
+    REQUIRE(utils::magnitudeTransverse(vector) == Catch::Approx(std::sqrt(5)));
+    REQUIRE(utils::magnitudeLongitudinal(vector) == utils::ValueType<TestType>(3));
+    REQUIRE(utils::vectorTransverse(vector) == TestType(1.0, 2.0, 0.0));
+    REQUIRE(utils::vectorLongitudinal(vector) == TestType(0.0, 0.0, 3.0));
+
+    const auto spherVec = utils::sphericalToVector(1, 1, 1);
+    // expected: (0.4546487134, 0.7080734183, 0.5403023059)
+    REQUIRE(spherVec.x == Catch::Approx(0.4546487134));
+    REQUIRE(spherVec.y == Catch::Approx(0.7080734183));
+    REQUIRE(spherVec.z == Catch::Approx(0.5403023059));
+
+    const auto otherVec = TestType(3, 4, 5);
+    REQUIRE(utils::angleBetween(otherVec, vector) == Catch::Approx(0.1862387)); // acos(26 / (sqrt(14) * sqrt(50)))
+  }
+}
---
+++ utils/include/edm4hep/utils/vector_utils.h
@@ -0,0 +1,273 @@
+#ifndef EDM4HEP_UTILS_VECTOR_HH
+#define EDM4HEP_UTILS_VECTOR_HH
+
+// These ultilies require concepts. If not available, use the fallback
+// vector_utils_legacy.h instead to capture most functionality.
+#if !__cpp_concepts
+#include <edm4hep/utils/vector_utils_legacy.h>
+#else
+
+#include <edm4hep/Vector3f.h>
+
+#include <cmath>
+
+namespace edm4hep {
+
+template <class V>
+concept VectorHasX = requires(V v) {
+  v.x;
+};
+
+template <class V>
+concept VectorHasY = requires(V v) {
+  v.y;
+};
+
+template <class V>
+concept VectorHasZ = requires(V v) {
+  v.z;
+};
+
+template <class V>
+concept VectorHasA = requires(V v) {
+  v.a;
+};
+
+template <class V>
+concept VectorHasB = requires(V v) {
+  v.b;
+};
+
+template <class V>
+concept ClassVector = requires(V v) {
+  v.x();
+};
+
+template <class V>
+concept Vector2D_XY = VectorHasX<V>&& VectorHasY<V> && !VectorHasZ<V> && !ClassVector<V>;
+
+template <class V>
+concept Vector2D_AB = VectorHasA<V>&& VectorHasB<V> && !VectorHasZ<V> && !ClassVector<V>;
+
+template <class V>
+concept Vector2D = Vector2D_XY<V> || Vector2D_AB<V>;
+
+template <class V>
+concept Vector3D = VectorHasX<V>&& VectorHasY<V>&& VectorHasZ<V> && !ClassVector<V>;
+
+template <class V>
+concept VectorND = Vector2D<V> || Vector3D<V>;
+
+template <class V>
+concept VectorND_XYZ = Vector2D_XY<V> || Vector3D<V>;
+
+namespace utils {
+  // Utility getters to accomodate different vector types
+  template <VectorND_XYZ V>
+  constexpr auto vector_x(const V& v) {
+    return v.x;
+  }
+
+  template <VectorND_XYZ V>
+  constexpr auto vector_y(const V& v) {
+    return v.y;
+  }
+
+  template <Vector3D V>
+  constexpr auto vector_z(const V& v) {
+    return v.z;
+  }
+
+  template <Vector2D_AB V>
+  constexpr auto vector_x(const V& v) {
+    return v.a;
+  }
+
+  template <Vector2D_AB V>
+  constexpr auto vector_y(const V& v) {
+    return v.b;
+  }
+
+  template <Vector2D V>
+  constexpr auto vector_z(const V&) {
+    return 0;
+  }
+
+  namespace detail {
+    /// Helper struct to determine the underlying type of edm4hep vector types
+    template <typename V>
+    struct ValueTypeHelper {
+      using type = decltype(vector_x(std::declval<V>()));
+    };
+  } // namespace detail
+
+  /// Type alias that returns the underlying type of edm4hep
+  template <typename V>
+  using ValueType = typename detail::ValueTypeHelper<V>::type;
+
+  // inline edm4hep::Vector2f VectorFromPolar(const double r, const double theta)
+  // {
+  //  return {r * sin(theta), r * cos(theta)};
+  //}
+
+} // namespace utils
+
+/// A vector that uses floating point numbers to represent its members
+template <typename V>
+concept FloatVectorND = std::floating_point<utils::ValueType<V>> && (Vector2D<V> || Vector3D<V>);
+
+namespace utils {
+  inline double etaToAngle(const double eta) {
+    return std::atan(std::exp(-eta)) * 2.;
+  }
+
+  inline double angleToEta(const double theta) {
+    return -std::log(std::tan(0.5 * theta));
+  }
+
+  template <Vector3D V = edm4hep::Vector3f>
+  V sphericalToVector(const double r, const double theta, const double phi) {
+    using FloatType = ValueType<V>;
+    const double sth = sin(theta);
+    const double cth = cos(theta);
+    const double sph = sin(phi);
+    const double cph = cos(phi);
+    const FloatType x = r * sth * cph;
+    const FloatType y = r * sth * sph;
+    const FloatType z = r * cth;
+    return {x, y, z};
+  }
+
+  template <Vector3D V>
+  double anglePolar(const V& v) {
+    return std::atan2(std::hypot(vector_x(v), vector_y(v)), vector_z(v));
+  }
+
+  template <VectorND V>
+  double angleAzimuthal(const V& v) {
+    return std::atan2(vector_y(v), vector_x(v));
+  }
+
+  template <Vector3D V>
+  double eta(const V& v) {
+    return angleToEta(anglePolar(v));
+  }
+
+  template <VectorND V>
+  double magnitude(const V& v) {
+    return std::hypot(vector_x(v), vector_y(v), vector_z(v));
+  }
+
+  template <Vector3D V>
+  double magnitudeTransverse(const V& v) {
+    return std::hypot(vector_x(v), vector_y(v));
+  }
+
+  template <Vector3D V>
+  double magnitudeLongitudinal(const V& v) {
+    return vector_z(v);
+  }
+
+  template <FloatVectorND V>
+  V normalizeVector(const V& v, double norm = 1.) {
+    const double old = magnitude(v);
+    if (old == 0) {
+      return v;
+    }
+    return (norm > 0) ? v * norm / old : v * 0;
+  }
+
+  template <Vector3D V>
+  constexpr V vectorTransverse(const V& v) {
+    return {vector_x(v), vector_y(v), 0};
+  }
+
+  template <Vector3D V>
+  constexpr V vectorLongitudinal(const V& v) {
+    return {0, 0, vector_z(v)};
+  }
+
+  // Two vector functions
+  template <VectorND V>
+  double angleBetween(const V& v1, const V& v2) {
+    const double dot = v1 * v2;
+    if (dot == 0) {
+      return 0.;
+    }
+    return acos(dot / (magnitude(v1) * magnitude(v2)));
+  }
+
+  // Project v onto v1
+  template <VectorND V>
+  double projection(const V& v, const V& v1) {
+    const double norm = magnitude(v1);
+    if (norm == 0) {
+      return magnitude(v);
+    }
+    return v * v1 / norm;
+  }
+
+} // namespace utils
+
+template <edm4hep::Vector2D V>
+inline constexpr V operator+(const V& v1, const V& v2) {
+  return {edm4hep::utils::vector_x(v1) + edm4hep::utils::vector_x(v2),
+          edm4hep::utils::vector_y(v1) + edm4hep::utils::vector_y(v2)};
+}
+
+template <edm4hep::Vector3D V>
+inline constexpr V operator+(const V& v1, const V& v2) {
+  return {edm4hep::utils::vector_x(v1) + edm4hep::utils::vector_x(v2),
+          edm4hep::utils::vector_y(v1) + edm4hep::utils::vector_y(v2),
+          edm4hep::utils::vector_z(v1) + edm4hep::utils::vector_z(v2)};
+}
+
+template <edm4hep::Vector2D V>
+inline constexpr auto operator*(const V& v1, const V& v2) {
+  return edm4hep::utils::vector_x(v1) * edm4hep::utils::vector_x(v2) +
+      edm4hep::utils::vector_y(v1) * edm4hep::utils::vector_y(v2);
+}
+
+template <edm4hep::Vector3D V>
+inline constexpr auto operator*(const V& v1, const V& v2) {
+  return edm4hep::utils::vector_x(v1) * edm4hep::utils::vector_x(v2) +
+      edm4hep::utils::vector_y(v1) * edm4hep::utils::vector_y(v2) +
+      edm4hep::utils::vector_z(v1) * edm4hep::utils::vector_z(v2);
+}
+
+template <edm4hep::Vector2D V>
+inline constexpr V operator*(const double d, const V& v) {
+  using VT = edm4hep::utils::ValueType<V>;
+  const VT x = d * edm4hep::utils::vector_x(v);
+  const VT y = d * edm4hep::utils::vector_y(v);
+  return {x, y};
+}
+
+template <edm4hep::Vector3D V>
+inline constexpr V operator*(const double d, const V& v) {
+  using VT = edm4hep::utils::ValueType<V>;
+  const VT x = d * edm4hep::utils::vector_x(v);
+  const VT y = d * edm4hep::utils::vector_y(v);
+  const VT z = d * edm4hep::utils::vector_z(v);
+  return {x, y, z};
+}
+
+template <edm4hep::VectorND V>
+inline constexpr V operator*(const V& v, const double d) {
+  return d * v;
+}
+
+template <edm4hep::VectorND V>
+inline constexpr V operator-(const V& v1, const V& v2) {
+  return v1 + (-1. * v2);
+}
+
+template <edm4hep::VectorND V>
+inline constexpr V operator/(const V& v, const double d) {
+  return (1. / d) * v;
+}
+
+} // namespace edm4hep
+
+#endif
+#endif
