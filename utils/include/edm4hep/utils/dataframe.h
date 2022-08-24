ae637968060744d027360beeeea44c522bc75e1a
Thomas Madlener <thomas.madlener@desy.de>
2022-08-24 16:04:34 +0200

    Add minimal documentation to utilities

--- utils/include/edm4hep/utils/dataframe.h
+++ utils/include/edm4hep/utils/dataframe.h
@@ -5,15 +5,19 @@
 
 namespace edm4hep::utils {
 
+/// Get the pt of the passed particle / datatypes
 template <typename T>
 ROOT::VecOps::RVec<float> pt(ROOT::VecOps::RVec<T> const& in);
 
+/// Get eta of the passed particles / datatype momenta
 template <typename T>
 ROOT::VecOps::RVec<float> eta(ROOT::VecOps::RVec<T> const& in);
 
+/// Get cos(theta) of the passed particles / datatype momenta
 template <typename T>
 ROOT::VecOps::RVec<float> cos_theta(ROOT::VecOps::RVec<T> const& in);
 
+/// Get r of the passed particle / datatype positions
 template <typename T>
 ROOT::VecOps::RVec<float> r(ROOT::VecOps::RVec<T> const& in);
 
