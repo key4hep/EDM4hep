9f9600dca5f6145a907120960c3a769c906a4fbb
Juraj Smiesko <34742917+kjvbrt@users.noreply.github.com>
2022-10-18 16:41:52 +0200

    Adding dummy EDM4hep .root -> Phoenix .json converter (#163)
    * Dummy conversion of calo clusters
    
    * Using cluster collection
    
    * Setting RPATH
    
    * Using new ability of podio to generate json
    
    * Adding core edm4hep agains nlohmann_json
    
    * Looping through all collections in the file
    
    * Add collection types
    
    * Add n events max
    
    * Adding more collection types and addressing the comments
    
    * Workaround to be able to install the executable

---
+++ tools/include/edm4hep2json.hxx
@@ -0,0 +1,265 @@
+#ifndef EDM4HEP_TO_JSON_H__
+#define EDM4HEP_TO_JSON_H__
+
+// event data model
+#include "edm4hep/EventHeaderCollection.h"
+#include "edm4hep/MCParticleCollection.h"
+#include "edm4hep/MCRecoParticleAssociationCollection.h"
+#include "edm4hep/VertexCollection.h"
+#include "edm4hep/TrackCollection.h"
+#include "edm4hep/TrackerHitCollection.h"
+#include "edm4hep/TrackerHitCollection.h"
+#include "edm4hep/SimTrackerHitCollection.h"
+#include "edm4hep/CalorimeterHitCollection.h"
+#include "edm4hep/SimCalorimeterHitCollection.h"
+#include "edm4hep/ClusterCollection.h"
+#include "edm4hep/CalorimeterHitCollection.h"
+#include "edm4hep/ReconstructedParticleCollection.h"
+#include "edm4hep/ParticleIDCollection.h"
+
+// podio specific includes
+#include "podio/EventStore.h"
+
+// JSON
+#include "nlohmann/json.hpp"
+
+// STL
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <exception>
+#include <cassert>
+
+
+template<typename PodioStoreT>
+nlohmann::json processEvent(PodioStoreT& store,
+                            std::vector<std::string>& collList,
+                            bool verboser,
+                            unsigned eventNum) {
+  const podio::CollectionBase* coll;
+  nlohmann::json jsonDict = {
+    {"edm4hepVersion", "0.7.0"}
+  };
+
+  for (unsigned i = 0; i < collList.size(); ++i) {
+    int err = store.template get<podio::CollectionBase>(collList[i], coll);
+    if (!err) {
+      continue;
+    }
+
+    if (coll->getTypeName() == "edm4hep::EventHeaderCollection") {
+      auto& eventCollection =
+          store.template get<edm4hep::EventHeaderCollection>(collList[i]);
+      nlohmann::json jsonColl {
+        {collList[i], {{"collection", eventCollection},
+                       {"collID", coll->getID()},
+                       {"collType", coll->getTypeName()}},
+        }
+      };
+      jsonDict.insert(jsonColl.begin(), jsonColl.end());
+    } else if (coll->getTypeName() == "edm4hep::MCParticleCollection") {
+      auto& particleCollection =
+          store.template get<edm4hep::MCParticleCollection>(collList[i]);
+      nlohmann::json jsonColl {
+        {collList[i], {{"collection", particleCollection},
+                       {"collID", coll->getID()},
+                       {"collType", coll->getTypeName()}},
+        }
+      };
+      jsonDict.insert(jsonColl.begin(), jsonColl.end());
+    } else if (coll->getTypeName() == "edm4hep::MCRecoParticleAssociationCollection") {
+      auto& assocCollection =
+          store.template get<edm4hep::MCRecoParticleAssociationCollection>(collList[i]);
+      nlohmann::json jsonColl {
+        {collList[i], {{"collection", assocCollection},
+                       {"collID", coll->getID()},
+                       {"collType", coll->getTypeName()}},
+        }
+      };
+      jsonDict.insert(jsonColl.begin(), jsonColl.end());
+    } else if (coll->getTypeName() == "edm4hep::VertexCollection") {
+      auto& vertexCollection =
+          store.template get<edm4hep::VertexCollection>(collList[i]);
+      nlohmann::json jsonColl {
+        {collList[i], {{"collection", vertexCollection},
+                       {"collID", coll->getID()},
+                       {"collType", coll->getTypeName()}}
+        }
+      };
+      jsonDict.insert(jsonColl.begin(), jsonColl.end());
+    } else if (coll->getTypeName() == "edm4hep::TrackCollection") {
+      auto& trackCollection =
+          store.template get<edm4hep::TrackCollection>(collList[i]);
+      nlohmann::json jsonColl {
+        {collList[i], {{"collection", trackCollection},
+                       {"collID", coll->getID()},
+                       {"collType", coll->getTypeName()}}
+        }
+      };
+      jsonDict.insert(jsonColl.begin(), jsonColl.end());
+    } else if (coll->getTypeName() == "edm4hep::TrackerHitCollection") {
+      auto& hitCollection =
+          store.template get<edm4hep::TrackerHitCollection>(collList[i]);
+      nlohmann::json jsonColl {
+        {collList[i], {{"collection", hitCollection},
+                       {"collID", coll->getID()},
+                       {"collType", coll->getTypeName()}}
+        }
+      };
+      jsonDict.insert(jsonColl.begin(), jsonColl.end());
+    } else if (coll->getTypeName() == "edm4hep::SimTrackerHitCollection") {
+      auto& hitCollection =
+          store.template get<edm4hep::SimTrackerHitCollection>(collList[i]);
+      nlohmann::json jsonColl {
+        {collList[i], {{"collection", hitCollection},
+                       {"collID", coll->getID()},
+                       {"collType", coll->getTypeName()}}
+        }
+      };
+      jsonDict.insert(jsonColl.begin(), jsonColl.end());
+    } else if (coll->getTypeName() == "edm4hep::CalorimeterHitCollection") {
+      auto& hitCollection =
+          store.template get<edm4hep::CalorimeterHitCollection>(collList[i]);
+      nlohmann::json jsonColl {
+        {collList[i], {{"collection", hitCollection},
+                       {"collID", coll->getID()},
+                       {"collType", coll->getTypeName()}}
+        }
+      };
+      jsonDict.insert(jsonColl.begin(), jsonColl.end());
+    } else if (coll->getTypeName() == "edm4hep::SimCalorimeterHitCollection") {
+      auto& hitCollection =
+          store.template get<edm4hep::SimCalorimeterHitCollection>(collList[i]);
+      nlohmann::json jsonColl {
+        {collList[i], {{"collection", hitCollection},
+                       {"collID", coll->getID()},
+                       {"collType", coll->getTypeName()}}
+        }
+      };
+      jsonDict.insert(jsonColl.begin(), jsonColl.end());
+    } else if (coll->getTypeName() == "edm4hep::ClusterCollection") {
+      auto& clusterCollection =
+          store.template get<edm4hep::ClusterCollection>(collList[i]);
+      nlohmann::json jsonColl {
+        {collList[i], {{"collection", clusterCollection},
+                       {"collID", coll->getID()},
+                       {"collType", coll->getTypeName()}}
+        }
+      };
+      jsonDict.insert(jsonColl.begin(), jsonColl.end());
+    } else if (coll->getTypeName() == "edm4hep::ReconstructedParticleCollection") {
+      auto& recoParticleCollection =
+          store.template get<edm4hep::ReconstructedParticleCollection>(collList[i]);
+      nlohmann::json jsonColl {
+        {collList[i], {{"collection", recoParticleCollection},
+                       {"collID", coll->getID()},
+                       {"collType", coll->getTypeName()}}
+        }
+      };
+      jsonDict.insert(jsonColl.begin(), jsonColl.end());
+    } else if (coll->getTypeName() == "edm4hep::ParticleIDCollection") {
+      auto& particleIDCollection =
+          store.template get<edm4hep::ParticleIDCollection>(collList[i]);
+      nlohmann::json jsonColl {
+        {collList[i], {{"collection", particleIDCollection},
+                       {"collID", coll->getID()},
+                       {"collType", coll->getTypeName()}}
+        }
+      };
+      jsonDict.insert(jsonColl.begin(), jsonColl.end());
+    } else {
+      std::cout << "WARNING: Collection type not recognized!\n"
+                << "         " << coll->getTypeName() << "\n";
+    }
+  }
+
+  return jsonDict;
+}
+
+
+template<typename PodioStoreT>
+void printCollTypes(PodioStoreT& store,
+                    std::vector<std::string>& collList) {
+  const podio::CollectionBase* coll;
+
+  std::cout << "INFO: Converting collections:\n";
+
+  for (unsigned i = 0; i < collList.size(); ++i) {
+    int err = store.template get<podio::CollectionBase>(collList[i], coll);
+    if (!err) {
+        std::cout << "WARNING: Something went wrong, ignoring collection:\n"
+                  << "         " << collList[i] << "\n";
+
+      continue;
+    }
+
+    std::cout << "       - " << coll->getID() << " | " << collList[i]
+              << " | " << coll->getTypeName() << "\n";
+  }
+}
+
+
+std::vector<std::string> splitString(const std::string& inString) {
+  std::vector<std::string> outString;
+  std::stringstream sStream(inString);
+
+  while (sStream.good()) {
+    std::string subString;
+    getline(sStream, subString, ',');
+    outString.emplace_back(subString);
+  }
+
+  return outString;
+}
+
+
+template<typename ReaderT>
+void read_events(const std::string& filename,
+                 const std::string& jsonFile,
+                 const std::string& requestedCollections,
+                 int nEventsMax = -1,
+                 bool verboser = false) {
+  ReaderT reader;
+  reader.openFile(filename);
+
+  auto store = podio::EventStore();
+  store.setReader(&reader);
+
+  nlohmann::json allEventsDict;
+
+  auto collList = splitString(requestedCollections);
+
+  if (verboser) {
+    printCollTypes(store, collList);
+  }
+
+  unsigned nEvents = reader.getEntries();
+  if (nEventsMax > 0) {
+    if ((unsigned) nEventsMax < nEvents) {
+      nEvents = nEventsMax;
+    }
+  }
+  for (unsigned i = 0; i < nEvents; ++i) {
+    if (verboser && i % 1000 == 0) {
+      std::cout << "INFO: Reading event " << i << std::endl;
+    }
+    auto eventDict = processEvent(store, collList, verboser, i);
+    allEventsDict["Event " + std::to_string(i)] = eventDict;
+    store.clear();
+    reader.endOfEvent();
+  }
+  reader.closeFile();
+
+  std::ofstream outFile(jsonFile);
+  if (outFile.is_open()) {
+    outFile << std::setw(2) << allEventsDict << std::endl;
+    outFile.close();
+  }
+
+  if (verboser) {
+    std::cout << "INFO: Result of the conversion written to:\n"
+              << "      " << jsonFile << std::endl;
+  }
+}
+
+#endif /* EDM4HEP_TO_JSON_H__ */
---
+++ tools/src/edm4hep2json.cxx
@@ -0,0 +1,107 @@
+// std
+#include <filesystem>
+
+// EDM4hep
+#include "edm4hep2json.hxx"
+
+// ROOT
+#include "podio/ROOTReader.h"
+
+// *nix
+#include <getopt.h>
+
+void printHelp() {
+  std::cout << "Usage: [olnvh] FILEPATH\n"
+            << "  -o/--out-file           output file path\n"
+            << "                            default: events.edm4hep.json\n"
+            << "  -l/--coll-list          Comma separated list of collections "
+                                         "to be converted\n"
+            << "  -n/--nevents            number of events to be precessed\n"
+            << "  -v/--verbose            be more verbose\n"
+            << "  -h/--help               show this help message"
+            << std::endl;
+}
+
+int main(int argc, char** argv) {
+  std::filesystem::path inFile;
+  std::filesystem::path outFile;
+  std::string requestedCollections;
+  bool verboser = false;
+  int nEventsMax = -1;
+
+  const char* const short_opts = "o:l:n:vh";
+  const option long_opts[] = {
+    {"out-file", required_argument, nullptr, 'o'},
+    {"coll-list", required_argument, nullptr, 'l'},
+    {"nevents", required_argument, nullptr, 'n'},
+    {"verbose", no_argument, nullptr, 'v'},
+    {"help", no_argument, nullptr, 'h'},
+    {nullptr, no_argument, nullptr, 0}
+  };
+
+  while (true) {
+    const auto opt = getopt_long(argc, argv, short_opts, long_opts, 0);
+
+    if (opt == -1) {
+      break;
+    }
+
+    switch (opt) {
+      case 'i':
+        inFile = std::filesystem::path(optarg);
+        break;
+      case 'o':
+        outFile = std::filesystem::path(optarg);
+        break;
+      case 'l':
+        requestedCollections = std::string(optarg);
+        break;
+      case 'n':
+        nEventsMax = std::stoi(optarg);
+        break;
+      case 'v':
+        verboser = true;
+        break;
+      case 'h':
+      case '?':
+      default:
+        printHelp();
+        return EXIT_SUCCESS;
+    }
+  }
+
+  for (int i = optind; i < argc; ++i) {
+    inFile = std::string(argv[i]);
+  }
+
+  if (inFile.empty()) {
+    std::cout << "ERROR: Input .root file not provided!" << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  if (!std::filesystem::exists(inFile)) {
+    std::cout << "ERROR: Input .root file can't be read!" << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  if (requestedCollections.empty()) {
+    requestedCollections =
+        "GenParticles,BuildUpVertices,SiTracks,PandoraClusters,VertexJets,EventHeader";
+    if (verboser) {
+      std::cout << "DEBUG: Using default collection to convert:\n"
+                << "       " << requestedCollections << std::endl;
+    }
+  }
+
+  if (outFile.empty()) {
+    outFile = inFile.filename().replace_extension("edm4hep.json");
+  }
+
+  read_events<podio::ROOTReader>(inFile,
+                                 outFile,
+                                 requestedCollections,
+                                 nEventsMax,
+                                 verboser);
+
+  return EXIT_SUCCESS;
+}
