7bcfe36d6ec8e8ec6b15923e772cae634301ccb6
jmcarcell <jmcarcell@users.noreply.github.com>
2023-05-07 10:15:21 +0200

    Don't use importlib

--- python/edm4hep/datamodel.py
+++ python/edm4hep/datamodel.py
@@ -1,19 +1,16 @@
-import importlib
 import ROOT
 res = ROOT.gSystem.Load('libedm4hepDict.so')
 if res < 0:
     raise RuntimeError('Failed to load libedm4hepDict.so')
+from ROOT import edm4hep
 
 
 class Datamodel:
     """
     Factory class to dynamically serve EDM4hep classes
     """
-    def __init__(self):
-        self.edm4hep = importlib.import_module('ROOT').edm4hep
-
     def __getattr__(self, name):
         try:
-            return getattr(self.edm4hep, name)
+            return getattr(edm4hep, name)
         except AttributeError:
             raise AttributeError(f'EDM4hep has no class named {name}')
