125f48d124e29613949c21259e1b5beeac45d63f
jmcarcell <jmcarcell@users.noreply.github.com>
2023-06-30 10:27:18 +0200

    Improve python API

--- python/edm4hep/__init__.py
+++ python/edm4hep/__init__.py
@@ -1,6 +1,27 @@
 """Python module for the EDM4HEP datamodel and utilities."""
+import sys
 
-from .datamodel import Datamodel, Utils
 from .__version__ import __version__
-edm4hep = Datamodel()
-utils = Utils()
+import ROOT
+res = ROOT.gSystem.Load('libedm4hepDict.so')
+if res < 0:
+    raise RuntimeError('Failed to load libedm4hepDict.so')
+
+res = ROOT.gSystem.Load('libedm4hepRDF.so')
+if res < 0:
+    raise RuntimeError('Failed to load libedm4hepRDF.so')
+
+res = ROOT.gInterpreter.LoadFile('edm4hep/utils/kinematics.h')
+if res != 0:
+    raise RuntimeError('Failed to load kinematics.h')
+
+res = ROOT.gInterpreter.LoadFile('edm4hep/utils/dataframe.h')
+if res != 0:
+    raise RuntimeError('Failed to load dataframe.h')
+from ROOT import edm4hep
+
+# Make TAB completion work for utils
+setattr(edm4hep, 'utils', edm4hep.utils)
+
+# Make `import edm4hep` work
+sys.modules['edm4hep'] = edm4hep
