f2bc5e5aa61418c7953090a21636d5b1d0556bd2
jmcarcell <jmcarcell@users.noreply.github.com>
2023-07-14 14:56:21 +0200

    Rename CMAKE_BINARY_DIR to PROJECT_BINARY_DIR

--- cmake/EDM4HEPUninstall.cmake
+++ cmake/EDM4HEPUninstall.cmake
@@ -33,14 +33,14 @@
 if (NOT TARGET uninstall)
     configure_file(
       "${CMAKE_CURRENT_LIST_DIR}/EDM4HEP_uninstall.cmake.in"
-      "${CMAKE_BINARY_DIR}/EDM4HEP_uninstall.cmake"
+      "${PROJECT_BINARY_DIR}/EDM4HEP_uninstall.cmake"
         IMMEDIATE
         @ONLY
     )
 
     add_custom_target(uninstall
-      COMMAND "${CMAKE_COMMAND}" -P "${CMAKE_BINARY_DIR}/EDM4HEP_uninstall.cmake"
-      WORKING_DIRECTORY "${CMAKE_BINARY_DIR}"
+      COMMAND "${CMAKE_COMMAND}" -P "${PROJECT_BINARY_DIR}/EDM4HEP_uninstall.cmake"
+      WORKING_DIRECTORY "${PROJECT_BINARY_DIR}"
     )
 endif()
 
