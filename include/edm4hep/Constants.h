ca6396faa9ac3e20b69dac490cdbe0d66b97336e
jmcarcell <jmcarcell@users.noreply.github.com>
2024-01-08 09:50:21 +0100

    Update license year; previous commit: Dox fix (#244)

--- include/edm4hep/Constants.h
+++ include/edm4hep/Constants.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014-2023 Key4hep-Project.
+ * Copyright (c) 2014-2024 Key4hep-Project.
  *
  * This file is part of Key4hep.
  * See https://key4hep.github.io/key4hep-doc/ for further info.
