b628e084b76430621faf472e24d32184d4bf37d0
jmcarcell <jmcarcell@users.noreply.github.com>
2024-02-06 18:31:15 +0100

    Change ROOTFrame{Writer,Reader} to ROOT{Writer,Reader} when possible

--- test/read_events.cc
+++ test/read_events.cc
@@ -1,8 +1,16 @@
 #include "read_events.h"
+#if PODIO_VERSION_MAJOR > 0 || (PODIO_VERSION_MAJOR == 0 && PODIO_VERSION_MINOR >= 99)
+#include "podio/ROOTReader.h"
+#else
 #include "podio/ROOTFrameReader.h"
+#endif
 
 int main() {
+#if PODIO_VERSION_MAJOR > 0 || (PODIO_VERSION_MAJOR == 0 && PODIO_VERSION_MINOR >= 99)
+  read_events<podio::ROOTReader>("edm4hep_events.root");
+#else
   read_events<podio::ROOTFrameReader>("edm4hep_events.root");
+#endif
 
   return 0;
 }
