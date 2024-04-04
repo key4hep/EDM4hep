50ca26efdc194cf49e33184f8d3e68432ea379f7
tmadlener <thomas.madlener@desy.de>
2024-04-04 13:22:35 +0200

    Also use non-deprecated version of SIOReader

--- test/read_events_sio.cc
+++ test/read_events_sio.cc
@@ -1,9 +1,17 @@
 #include "read_events.h"
 
+#include "podio/podioVersion.h"
+#if PODIO_BUILD_VERSION >= PODIO_VERSION(0, 99, 0)
+#include "podio/SIOReader.h"
+#else
 #include "podio/SIOFrameReader.h"
+namespace podio {
+using SIOReader = podio::SIOFrameReader;
+}
+#endif
 
 int main(int, char**) {
-  read_events<podio::SIOFrameReader>("edm4hep_events.sio");
+  read_events<podio::SIOReader>("edm4hep_events.sio");
 
   return 0;
 }
