e27064f91cb1a8cdcbb840c6ff794dd1096f464a
Thomas Madlener <thomas.madlener@desy.de>
2024-04-04 14:49:53 +0200

    Use non-deprecated versions of SIO Reader/Writer (#293)

--- test/write_events_sio.cc
+++ test/write_events_sio.cc
@@ -1,8 +1,16 @@
 #include "write_events.h"
 
+#include "podio/podioVersion.h"
+#if PODIO_BUILD_VERSION >= PODIO_VERSION(0, 99, 0)
+#include "podio/SIOWriter.h"
+#else
 #include "podio/SIOFrameWriter.h"
+namespace podio {
+using SIOWriter = podio::SIOFrameWriter;
+}
+#endif
 
 int main(int, char**) {
-  write<podio::SIOFrameWriter>("edm4hep_events.sio");
+  write<podio::SIOWriter>("edm4hep_events.sio");
   return 0;
 }
