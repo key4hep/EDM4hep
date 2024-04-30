0a07c65317e9fbd58af667be0c87e3dd4df6a995
tmadlener <thomas.madlener@desy.de>
2024-04-30 13:05:52 +0200

    Add constructor from initializer lists for cov matrix components

--- test/utils/test_covmatrix_utils.cpp
+++ test/utils/test_covmatrix_utils.cpp
@@ -117,4 +117,11 @@ TEST_CASE("TrackerHit3D covariance", "[cov_matrix_utils]") {
 
   auto hit = edm4hep::TrackerHit3D(trackerHit);
   REQUIRE(hit.getCovMatrix(edm4hep::Cartesian::x, edm4hep::Cartesian::z) == 3.14f);
+
+  trackerHit.setCovMatrix({1.f, 2.f, 3.f, 4.f, 5.f, 6.f});
+  REQUIRE(trackerHit.getCovMatrix() == std::array{1.f, 2.f, 3.f, 4.f, 5.f, 6.f});
+
+  const std::array arrValues = {6.f, 5.f, 4.f, 3.f, 2.f, 1.f};
+  trackerHit.setCovMatrix(arrValues);
+  REQUIRE(trackerHit.getCovMatrix() == std::array{6.f, 5.f, 4.f, 3.f, 2.f, 1.f});
 }
