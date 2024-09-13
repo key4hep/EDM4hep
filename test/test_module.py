3bb360962a3310d51c8707f1937adb1639c86699
Mateusz Jakub Fila <mateusz.jakub.fila@cern.ch>
2024-09-13 17:25:40 +0200

    test python package attributes

---
+++ test/test_module.py
@@ -0,0 +1,8 @@
+import edm4hep
+
+assert edm4hep.__version__
+assert edm4hep.__name__ == "edm4hep"
+assert edm4hep.__spec__
+assert edm4hep.__path__
+assert edm4hep.__file__
+assert "utils" in dir(edm4hep)
