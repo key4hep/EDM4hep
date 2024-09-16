223e72cbe512c58b712b3d27c4b5eb93479cdfa2
Mateusz Jakub Fila <mateusz.jakub.fila@cern.ch>
2024-09-16 09:12:40 +0200

    test values of module attributes

--- test/test_module.py
+++ test/test_module.py
@@ -1,8 +1,29 @@
-import edm4hep
+#!/usr/bin/env python3
 
-assert edm4hep.__version__
-assert edm4hep.__name__ == "edm4hep"
-assert edm4hep.__spec__
-assert edm4hep.__path__
-assert edm4hep.__file__
+"""Check the attributes of edm4hep package"""
+
+import edm4hep
+from pathlib import Path
+from ROOT import gInterpreter
+
+if gInterpreter.LoadFile("edm4hep/EDM4hepVersion.h") != 0:
+    raise ImportError("Error while loading file edm4hep/EDM4hepVersion.h")
+
+# __version__
+version = edm4hep.version.build_version
+assert edm4hep.__version__ == f"{version.major}.{version.minor}.{version.patch}"
+# __name__
+name = "edm4hep"
+assert edm4hep.__name__ == name
+# __file__
+expected_origin = str(Path(__file__).parent.parent / "python" / "edm4hep" / "__init__.py")
+assert edm4hep.__file__ == expected_origin
+# __path__
+expected_path = [str(Path(expected_origin).parent)]
+assert edm4hep.__path__ == expected_path
+# __spec__
+assert edm4hep.__spec__.name == name
+assert edm4hep.__spec__.origin == expected_origin
+assert edm4hep.__spec__.submodule_search_locations == expected_path
+# utils
 assert "utils" in dir(edm4hep)
