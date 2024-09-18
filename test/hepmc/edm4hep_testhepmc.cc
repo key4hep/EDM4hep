b2c9858c512d21977bb307e43fd1253bfe7b68e0
Juan Miguel Carceller <22276694+jmcarcell@users.noreply.github.com>
2024-09-18 20:46:53 +0200

    Remove unused variable in edm4hep_testhepmc.cc (#372)

--- test/hepmc/edm4hep_testhepmc.cc
+++ test/hepmc/edm4hep_testhepmc.cc
@@ -109,7 +109,6 @@ int main() {
 
   // Part2: Convert the particles and write to file
   std::unordered_map<unsigned int, edm4hep::MutableMCParticle> hepmcToEdmMap;
-  unsigned int particle_counter{0};
   for (auto particle_i : evt->particles()) {
     std::cout << "Converting particle with PDG ID: " << particle_i->pdg_id() << std::endl;
     std::cout << "\t and id: " << particle_i->id() << std::endl;
@@ -143,8 +142,6 @@ int main() {
         hepmcToEdmMap[particle_i->id()].addToDaughters(hepmcToEdmMap[particle_daughter->id()]);
       }
     }
-
-    particle_counter++;
   }
 
   auto edm_particle_collection = edm4hep::MCParticleCollection();
