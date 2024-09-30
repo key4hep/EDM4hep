eb8592e4ec5fe294f27722ec8426461d97443c29
Mateusz Jakub Fila <37295697+m-fila@users.noreply.github.com>
2024-09-30 11:07:21 +0200

    add formatting and linting python in pre-commit (#370)
    * add ruff pre-commit
    
    * install ruff in pre-commit venv
    
    * add cmake target for ruff and ruff-format
    
    * fix formatting
    
    * fix linted warnings
    
    * be explicit about formatting options
    
    * target py311

--- test/test_rdf.py
+++ test/test_rdf.py
@@ -5,25 +5,31 @@ import edm4hep  # noqa: F401
 
 ROOT.EnableImplicitMT()
 
-print('Create RDataFrame ...')
-df = ROOT.RDataFrame('events', 'edm4hep_events.root')
+print("Create RDataFrame ...")
+df = ROOT.RDataFrame("events", "edm4hep_events.root")
 
-print('Apply selectors and define new branches ...')
-df2 = (df.Define('MCParticles_pt', 'edm4hep::utils::pt(MCParticles)')
-       .Define('MCParticles_eta', 'edm4hep::utils::eta(MCParticles)')
-       .Define('MCParticles_cosTheta', 'edm4hep::utils::cos_theta(MCParticles)')
-       .Define('SimTrackerHits_r', 'edm4hep::utils::r(SimTrackerHits)')
-       .Define('SimTrackerHits_pt', 'edm4hep::utils::pt(SimTrackerHits)')
-       .Define('TrackerHits_r', 'edm4hep::utils::r(TrackerHitPlanes)')
-       )
+print("Apply selectors and define new branches ...")
+df2 = (
+    df.Define("MCParticles_pt", "edm4hep::utils::pt(MCParticles)")
+    .Define("MCParticles_eta", "edm4hep::utils::eta(MCParticles)")
+    .Define("MCParticles_cosTheta", "edm4hep::utils::cos_theta(MCParticles)")
+    .Define("SimTrackerHits_r", "edm4hep::utils::r(SimTrackerHits)")
+    .Define("SimTrackerHits_pt", "edm4hep::utils::pt(SimTrackerHits)")
+    .Define("TrackerHits_r", "edm4hep::utils::r(TrackerHitPlanes)")
+)
 
-filename = 'edm4hep_events_py_rdf.root'
-print(f'Writing snapshot to disk ... \t{filename}')
+filename = "edm4hep_events_py_rdf.root"
+print(f"Writing snapshot to disk ... \t{filename}")
 
-df2.Snapshot('events', filename,
-             ['MCParticles_pt',
-              'MCParticles_eta',
-              'MCParticles_cosTheta',
-              'SimTrackerHits_r',
-              'SimTrackerHits_pt',
-              'TrackerHits_r'])
+df2.Snapshot(
+    "events",
+    filename,
+    [
+        "MCParticles_pt",
+        "MCParticles_eta",
+        "MCParticles_cosTheta",
+        "SimTrackerHits_r",
+        "SimTrackerHits_pt",
+        "TrackerHits_r",
+    ],
+)
