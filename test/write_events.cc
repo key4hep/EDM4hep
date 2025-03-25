65d81e0c151438d1047f3cb29915d790acf5e9b1
jmcarcell <jmcarcell@users.noreply.github.com>
2025-03-25 16:06:34 +0100

    Revert unintended changes in write_events.cc from 22f258d6

--- test/write_events.cc
+++ test/write_events.cc
@@ -1,16 +1,4 @@
 #include "write_events.h"
-
 #include "podio/ROOTWriter.h"
 
-#include "edm4hep/MCParticleCollection.h"
-
-int main(int, char*[]) {
-
-  edm4hep::MCParticleCollection mcparticles;
-  for (size_t i = 0; i < 10000000; ++i) {
-    edm4hep::MutableMCParticle particle;
-    mcparticles.push_back(particle);
-  }
-  // mcparticles.prepareForWrite();
-  // write<podio::ROOTWriter>("edm4hep_events.root");
-}
+int main(int, char*[]) { write<podio::ROOTWriter>("edm4hep_events.root"); }
