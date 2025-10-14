2ed1e9c4e6fb66b51a5e84bbb7c112fa6f3f9e06
Thomas Madlener <thomas.madlener@desy.de>
2025-10-14 15:52:52 +0200

    Shorten the merging script
    Tailor it to exactly the use case we have at the moment

--- edm4hep/schema_evolution/merge_selection_xml.py
+++ edm4hep/schema_evolution/merge_selection_xml.py
@@ -6,35 +6,27 @@ import xml.etree.ElementTree as ET
 import re
 
 
+def escape_name_content(match):
+    """Replace carrets in the regex group match with XML parsing compatible
+    representations"""
+    name_value = match.group(1)
+    escaped_value = name_value.replace("<", "&lt;").replace(">", "&gt;")
+    return f'name="{escaped_value}"'
+
+
 def escape_xml_content(content):
     """Escape < and > characters in XML content while preserving XML structure"""
     lines = content.split("\n")
     escaped_lines = []
 
     for line in lines:
-        # Skip XML declaration and root elements
-        stripped = line.strip()
-        if (
-            stripped.startswith("<?xml")
-            or stripped.startswith("<lcgdict")
-            or stripped.startswith("</lcgdict")
-            or stripped.startswith("<selection")
-            or stripped.startswith("</selection")
-        ):
-            escaped_lines.append(line)
+        # Assume that c++ template syntax only appears in class declarations
+        # which will have a name="XYX" somewhere. This also assumes that all of
+        # these xml tags are on one line!
+        if "name=" in line:
+            escaped_lines.append(re.sub(r'name="([^"]*)"', escape_name_content, line))
         else:
-            # For content lines, escape < and > that are not part of XML tags
-            # This is a simple approach - escape content within name attributes
-            if 'name="' in line:
-
-                def escape_name_content(match):
-                    name_value = match.group(1)
-                    escaped_value = name_value.replace("<", "&lt;").replace(">", "&gt;")
-                    return f'name="{escaped_value}"'
-
-                escaped_lines.append(re.sub(r'name="([^"]*)"', escape_name_content, line))
-            else:
-                escaped_lines.append(line)
+            escaped_lines.append(line)
 
     return "\n".join(escaped_lines)
 
@@ -58,19 +50,12 @@ with open(manual_file, "r", encoding="utf-8") as f:
 gen_tree = ET.fromstring(gen_content)
 manual_tree = ET.fromstring(manual_content)
 
-# For now merge only the selection parts
 gen_selection = gen_tree.find("selection")
 manual_selection = manual_tree.find("selection")
-
-
 for elem in manual_selection:
     gen_selection.append(elem)
 
 ET.indent(gen_tree, space="  ", level=0)
-output_content = ET.tostring(gen_tree, encoding="unicode")
-
-# Unescape the content back to original form
-output_content = unescape_xml_output(output_content)
-
+output_content = unescape_xml_output(ET.tostring(gen_tree, encoding="unicode"))
 with open(output_file, "w", encoding="utf-8") as f:
     f.write(output_content)
